315b5b65afc2a0125abdaa3592b2f3c035807a65
Valentin Bauer <e142525@student.tuwien.ac.at>
2018-01-11 16:22:14 +0100

    Change decimal separator in input format spec; Impl data generator

--- src/test/c/data_generator.c
+++ src/test/c/data_generator.c
@@ -16,51 +16,91 @@
 #include <time.h>
 #include <float.h>
 #include <unistd.h>
+#include <string.h>
+
+#define MAX_FILENAME_CHARS (256)
 
 extern char *optarg;
 extern int optind, opterr, optopt;
 
 /**
- * Generate a random floating point number from MIN to MAX
- * */
-double rand_double(double min, double max);
+ * Generate a random floating point number between MIN and MAX
+ * @param min
+ * @param max
+ * @return
+ */
+static double rand_double(double min, double max);
 
 /**
  * Print the usage message
  */
-void print_usage();
+static void print_usage();
 
 int main(int argc, char **argv)
 {
     int c;
     char *output_filename = "input.txt";
-    int processor_count = 48;
-    int x_dim = -1;
-    int y_dim = -1;
-    int z_dim = -1;
+    long processor_count = 48;
+    long x_dim = -1;
+    long y_dim = -1;
+    long z_dim = -1;
 
 
     while ((c = getopt(argc, argv, "o:p:x:y:z:")) != 1)
     {
-        switch (c) {
-            case o:
-
+        switch (c)
+        {
+            case 'o':
+                (void) strncpy(output_filename, optarg, strnlen(optarg, MAX_FILENAME_CHARS));
+                break;
+            case 'p':
+                p = strtol(optarg, NULL, 10);
+                break;
+            case 'x':
+                x_dim = strtol(optarg, NULL, 10);
+                break;
+            case 'y':
+                y_dim = strtol(optarg, NULL, 10);
+                break;
+            case 'z':
+                z_dim = strtol(optarg, NULL, 10);
                 break;
             default:
                 print_usage();
                 break;
         }
     }
+
+    if ((x_dim % p != 0)
+        || (y_dim % p != 0)
+        || (z_dim % p != 0))
+    {
+        fprintf(stderr, "Not all dimensions are evenly divisible by the number of processors!\n");
+        return -1;
+    }
+
+    FILE *fp = fopen("./input.txt", w);
+    (void) fprintf(fp, "%dl;%dl;%dl\n", x_dim, y_dim, z_dim);
+
+    long value_count = x_dim*y_dim*z_dim;
+
+    for (int i = 0; i<value_count; i++) {
+        (void) fprintf("%f\n", rand_double(-DBL_MAX, DBL_MAX));
+    }
+
+    (void) fclose(fp);
+
+    return 0;
 }
 
-double rand_double(double min, double max)
+static double rand_double(double min, double max)
 {
     double range = (max - min);
     double div = RAND_MAX / range;
     return min + (rand() / div);
 }
 
-void print_usage()
+static void print_usage()
 {
     (void) printf("Usage: data_generator [-h | -p <processor count> | -o <filename>] -x <X> -y <Y> -z <Z>");
 }
