51ad4f99c0b86dc2b5fd5047feb3f2c6dd1c2b86
Dardan <mail@dardan.im>
2018-01-12 23:10:57 +0100

    Implemented the stencil_plane functions in sequential.c

--- src/main/c/sequential/sequential.c
+++ src/main/c/sequential/sequential.c
@@ -1,13 +1,58 @@
 #include "sequential.h"
 #include "main/c/core/core.h"
 
-void stencil_plane(double **matrix, double *buffer_plane, int z) {
-    double* prev = matrix[z - 1];
-    double* curr = matrix[z];
-    double* futu = matrix[z + 1];
+#define ELEMENT(ARR, X, Y) ((ARR)[(X)*(Y) + (X)])
 
+void stencil_plane_7(struct pc_matrix *matrix, double *buffer_plane, int z) {
+    double* prev = matrix->arr[z - 1];
+    double* curr = matrix->arr[z];
+    double* futu = matrix->arr[z + 1];
 
+    for (int i = 1; i < matrix->x-1; ++i) {
+        for (int j = 1; j < matrix->y; ++j) {
+            double tmp = ELEMENT(curr, i, j);
+            tmp += ELEMENT(curr, i, j + 1);
+            tmp += ELEMENT(curr, i, j - 1);
+            tmp += ELEMENT(curr, i + 1, j);
+            tmp += ELEMENT(curr, i - 1, j);
+            tmp += ELEMENT(prev, i, j);
+            tmp += ELEMENT(futu, i, j);
+            tmp /= 7;
+            ELEMENT(buffer_plane, i, j) = tmp;
+        }
+    }
+}
 
+void stencil_plane_27(struct pc_matrix *matrix, double *buffer_plane, int z) {
+    double* prev = matrix->arr[z - 1];
+    double* curr = matrix->arr[z];
+    double* futu = matrix->arr[z + 1];
+
+    for (int i = 1; i < matrix->x-1; ++i) {
+        for (int j = 1; j < matrix->y; ++j) {
+            double tmp = 0;
+            for (int a = -1; a < 2; ++a) {
+                for (int b = -1; b < 2; ++b) {
+                    tmp += ELEMENT(prev, i+a, j+b);
+                }
+            }
+
+            for (int a = -1; a < 2; ++a) {
+                for (int b = -1; b < 2; ++b) {
+                    tmp += ELEMENT(curr, i+a, j+b);
+                }
+            }
+
+            for (int a = -1; a < 2; ++a) {
+                for (int b = -1; b < 2; ++b) {
+                    tmp += ELEMENT(futu, i+a, j+b);
+                }
+            }
+
+            tmp /= 27;
+            ELEMENT(buffer_plane, i, j) = tmp;
+        }
+    }
 }
 
 void run_stencil(struct pc_matrix *matrix) {
