d397fe2e5904803925c36ece6bfbf45f560f7517
Dardan <mail@dardan.im>
2018-01-12 20:57:44 +0100

    Wrote some dummy functions so that we can work seperately on them later

--- src/main/c/sequential/sequential.c
+++ src/main/c/sequential/sequential.c
@@ -1,1 +1,15 @@
 #include "sequential.h"
+#include "main/c/core/core.h"
+
+void stencil_plane(double **matrix, double *buffer_plane, int z) {
+    double* prev = matrix[z - 1];
+    double* curr = matrix[z];
+    double* futu = matrix[z + 1];
+
+
+
+}
+
+void run_stencil(struct pc_matrix *matrix) {
+    // MAKE SURE YOU START FROM 1 and end to N-1
+}
