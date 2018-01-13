c8d7485409ee3baa1cf501a8a2afd07c13272cec
Valentin Bauer <e1425252@student.tuwien.ac.at>
2018-01-14 00:13:33 +0100

    Impl cilk lib (untested)

---
+++ lib/cilk/pc_stencil/cilk_stencil.c
@@ -0,0 +1,68 @@
+#include <malloc.h>
+#include <cilk/cilk.h>
+#include "cilk_stencil.h"
+
+static void one_plane_pass_7_stencil(struct pc_matrix *matrix, double *pred, double *curr, double *succ, double *buff);
+
+void run_cilk_stencil_7(struct pc_matrix *matrix) {
+    double *updt_buff = malloc(matrix->x * matrix->y * sizeof(double));
+    double *calc_buff = malloc(matrix->x * matrix->y * sizeof(double));
+
+    for (int pass = 0; pass < ITERATION_COUNT; ++pass) {
+
+        for (int k = 1; k < matrix->z-1; ++k) {
+
+            double* prev = matrix->arr[k - 1];
+            double* curr = matrix->arr[k];
+            double* succ = matrix->arr[k + 1];
+
+            one_plane_pass_7_stencil(matrix, prev, curr, succ, calc_buff);
+
+            if (k == 1) {
+                double *tmp = calc_buff;
+                calc_buff = updt_buff;
+                updt_buff = tmp;
+            } else {
+                double *tmp = calc_buff;
+                calc_buff = prev;
+                matrix->arr[k - 1] = updt_buff;
+                updt_buff = tmp;
+            }
+        }
+
+        double *tmp = matrix->arr[matrix->z - 2];
+        matrix->arr[matrix->z - 2] = updt_buff;
+        updt_buff = tmp;
+    }
+
+    free(updt_buff);
+    free(calc_buff);
+}
+
+static void one_plane_pass_7_stencil(struct pc_matrix *matrix, double *pred, double *curr, double *succ, double *buff) {
+    int x = matrix->x;
+    int y = matrix->y;
+    int z = matrix->z;
+
+    // Copy boundary values
+    buff[0 : x] = curr[0 : x]; // top
+    buff[(y-1)*x : x] = curr[(y-1)*x : x]; // bottom
+    buff[0 : 1 : y] = curr[0 : 1 : y]; // left
+    buff[(x-2) : 1 : y] = curr[(x-2) : 1 : y]; // right
+
+    // Horizontally calculate lines (possibly in parallel
+    cilk_for (int j = 1; j < y - 1; ++j) {
+        int n = x-2;
+        int m = y-2;
+        // smash top, center and bottom line together
+        buff[j*x : n] = curr[(j-1)*x : n] + curr[(j+1)*x : n];
+        //smash previous and next line together
+        buff[j*x : n] = buff[j*x : n] + pred[j*x : n] + succ[j*x : n];
+
+        for (int i = 1; i < x - 1; ++i) {
+            buff[i*j + i] = buff[i*j + i] + curr[i*j + i-1] + curr[i*j + i+1];
+        }
+
+        buff[1 : n] = buff[1 : n] / 7.0;
+    }
+}
---
+++ lib/cilk/pc_stencil/cilk_stencil.h
@@ -0,0 +1,12 @@
+#ifndef PARCOMP_PROJECT_WS17_CILK_STENCIL_H
+#define PARCOMP_PROJECT_WS17_CILK_STENCIL_H
+
+#include "pc_stencil/core.h"
+
+/**
+ * Applies the 7-stencil to <matrix> ITERATOR_COUNT times
+ * @param matrix
+ */
+void run_cilk_stencil_7(struct pc_matrix *matrix);
+
+#endif //PARCOMP_PROJECT_WS17_CILK_STENCIL_H
